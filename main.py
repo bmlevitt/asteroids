74060e2385a319fa37dc060c2861fa9877642601
bmlevitt <hello@blevitt.com>
2025-02-11 17:55:05 -0800

    Add player movement and rendering with CircleShape base class

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     run_game = True
     print("Starting asteroids!")
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    while run_game == True:
+    while run_game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
-    # print (f"Screen width: {SCREEN_WIDTH}")
-    # print (f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
